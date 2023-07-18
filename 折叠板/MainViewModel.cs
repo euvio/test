55e563e10c576093091e75d2c2ba3ad35abdef09
euv <euv7@outlook.com>
2023-07-18 22:37:20 +0800

    添加折叠板

---
+++ 折叠板/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+
+namespace 折叠板
+{
+    public class ConfigItem
+    {
+        public string Description { get; set; }
+        public string JsonValue { get; set; }
+        public string Name { get; set; }
+        public Type Type => typeof(int);
+        public string Unit { get; set; }
+    }
+
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+        {
+            TreeNode root = new TreeNode();
+            root.Name = "Root";
+            root.Children = new List<TreeNode>();
+            root.Children.Add(new TreeNode() { Name = "A" });
+            root.Children.Add(new TreeNode()
+            {
+                Name = "B",
+                Children = new List<TreeNode>()
+                {
+                    new TreeNode()
+                    {
+                        Name = "2"
+                    }
+                }
+            });
+            root.Children.Add(new TreeNode()
+            {
+                Name = "C",
+                Children = new List<TreeNode>()
+                {
+                    new TreeNode()
+                    {
+                        Name = "3",
+                        Children = new List<TreeNode>()
+                        {
+                            new TreeNode(){Name = "c"}
+                        }
+                    }
+                }
+            });
+            Tree = new List<TreeNode>();
+            Tree.Add(root);
+        }
+
+        public IList<TreeNode> Tree { get; }
+    }
+
+    public class TreeNode
+    {
+        public IList<TreeNode> Children { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 折叠板/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace 折叠板
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
