9ee806b9083950ec43dbaf2560b72e0b346fe556
euv <euv7@outlook.com>
2023-07-29 11:36:25 +0800

    .

---
+++ 视图增删一行后台集合是否增删/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace 视图增删一行后台集合是否增删
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 视图增删一行后台集合是否增删/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 视图增删一行后台集合是否增删
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; } = new ObservableCollection<Product>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Products.Add(new Product() { Name = "牛奶" });
+            Products.Add(new Product() { Name = "香蕉" });
+            Products.Add(new Product() { Name = "可乐" });
+            listbox.ItemsSource = Products;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            //(CollectionViewSource.GetDefaultView(Products) as ListCollectionView).AddNewItem(new Product() { Name = "苹果" });
+            //var target = new Product() { Name = "可乐" };
+            //int index = ((CollectionViewSource.GetDefaultView(Products) as ListCollectionView)).IndexOf(target);
+            //((CollectionViewSource.GetDefaultView(Products) as ListCollectionView)).RemoveAt(0);
+
+            Product p = null;
+            foreach (var VARIABLE in (CollectionViewSource.GetDefaultView(Products) as ListCollectionView))
+            {
+                if (VARIABLE.Equals(new Product() { Name = "牛奶" }))
+                {
+                    p = VARIABLE as Product;
+                }
+            }
+
+            Products.Remove(p);
+        }
+
+        private void SEEButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            int a = Products.Count;
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            //return ReferenceEquals(obj, this);
+            if (obj == null) return false;
+
+            string name = obj.GetType().Name;
+            //if (name == "NamedObject") return false;
+            if (this.Name == (obj as Product).Name)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
