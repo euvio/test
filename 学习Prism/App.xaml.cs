795633f4096f5d0bda2e572fda4583b47c41e5c7
Bruce Lee <euv7@outlook.com>
2023-08-13 01:41:03 +0800

    .

--- 学习Prism/App.xaml.cs
+++ 学习Prism/App.xaml.cs
@@ -5,6 +5,12 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DryIoc;
+using Example;
+using Prism.DryIoc;
+using Prism.Events;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
 
 namespace 学习Prism
 {
@@ -13,5 +19,13 @@ namespace 学习Prism
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            ContainerLocator.SetContainerExtension(() => new DryIocContainerExtension());
+            ContainerLocator.Current.Register<IDialogService, DialogService>();
+            ContainerLocator.Current.Register<IEventAggregator, EventAggregator>();
+    
+            IContainerExtension<IContainer> a = new DryIocContainerExtension();
+        }
     }
 }
--- 学习Prism/MainViewModel.cs
+++ 学习Prism/MainViewModel.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Prism.Events;
+using Prism.Ioc;
 
 namespace 学习Prism
 {
@@ -27,51 +29,15 @@ namespace 学习Prism
 
         public MainViewModel()
         {
-            Command = new DelegateCommand(() =>
+            var a = ContainerLocator.Container.Resolve<IEventAggregator>();
+
+            var b = ContainerLocator.Container.Resolve<IContainerProvider>();
+
+            if (b == null)
             {
                 ;
-            },
-                () =>
-                {
-                    var ssss = Random.Shared.NextInt64() % 2 == 0;
-
-                    Console.WriteLine(ssss);
-                    return ssss;
-
-                }).ObservesProperty(() => S.MyProperty);
-
-
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    Thread.Sleep(1000);
-                    S.OOOO("MyProperty");
-                }
-            });
-
-
-        //DelegateCommand command1 = new DelegateCommand(Execute);
-        //// command1 命令源始终处于可触发状态，随时可以执行命令
-
-        //DelegateCommand command2 = new DelegateCommand(Execute, CanExecute);
-        //// 影响CanExecute返回值的变量(因素)的值发生变化时，必须调用command2.RaiseCanExecuteChanged()以刷新命令源的启禁用状态。
-
-        DelegateCommand command3 = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => Name)
-          .ObservesProperty(() => Age);
-
-        //// ObservesProperty()的作用就是当某个继承了INotifyPropertyChanged的对象发布了它的某个属性发生了变化时，立刻调用一次command3.RaiseCanExecuteChanged()以刷新命令源的启禁用状态。
-
-
-
-
-        //DelegateCommand command4 = new DelegateCommand(Execute).ObservesCanExecute(() => IsEnable);
-
-        //DelegateCommand command5 = new DelegateCommand(Execute).ObservesCanExecute(() => IsEnable).ObservesProperty(()=>Age);
-
-        //     // command.RaiseCanExecuteChanged();
-
-    }
+            }
+        }
 
         private bool CanExecute()
         {
